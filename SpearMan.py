2edecf966cba9bccd83b58bcdcfa488eef7fbb6f
Annika Veksø <anniika-xb@live.dk>
2020-09-28 15:07:10 +0200

    Maybe fixed merged?

--- SpearMan.py
+++ SpearMan.py
@@ -1,13 +1,8 @@
 import random
-from Person import *
 from Gladiators import *
 
 
 class SpearMan(Gladiators):  # the subclass of the gladiators, which is the type Spear warrior
-    weapon_type = 'humongous Spear'
-    # weapon_range = random.randint(3, 10)
-    weapon_dmg = int()
-    toughness = random.randint(50, 100)
 
-    def randomDamage(self):
-        self.weapon_dmg = random.randint(40, 60)
+    def __init__(self, name: str, age: int):
+        Gladiators.__init__(self, name, age, "Spear", random.randint(40, 60), random.randint(50, 100))
