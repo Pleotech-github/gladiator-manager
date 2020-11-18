8bc24182b24250e7ea4b91f36ac7f641f9bdc448
Christian <Christianchr89@outlook.dk>
2020-11-18 20:01:15 +0100

    Played around creating a client and server. Still work in progress.

--- Client.py
+++ Client.py
@@ -1,6 +1,14 @@
 import socket
 
+text_bytes = 8
+port = 12345
+unicode = 'UTF-8'
+server = "PLACE IP-ADDRESS HERE"
+adr_location = (server, port)
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client.connect(adr_location)
 
+"""
 class Client:
     server_socket = socket.socket()
     host = socket.gethostbyname("localhost")
@@ -9,3 +17,4 @@ class Client:
     server_socket.connect((host, port))
     print(server_socket.recv(1024))
     server_socket.close()
+"""
--- Server.py
+++ Server.py
@@ -1,6 +1,40 @@
 import socket
+import threading
+
+text_bytes = 8  # Number of bytes
+unicode = 'UTF-8'
+port = 12345
+ip_server = socket.gethostbyname(socket.gethostname())# Gets the information about client pc
+adr_location = (ip_server, port)
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)  # connects to the internet through TCP socket(SOCK_STREAM)
 
 
+def client_operation(connect, adr):  # connects link between clients and server
+    print(f"New connection {adr} joined")
+
+    linked = True
+    while linked:
+        linked_text_length = connect.recv(text_bytes).decode(
+            unicode)  # the amount of bytes that are being send to the client
+    if linked_text_length:
+        linked_text_length = int(linked_text_length)
+        message = connect.recv(linked_text_length).decode(unicode)
+        print(f"({adr}) {message}")
+        # maybe add disconnection option?
+        connect.close()
+
+
+def client_running():  # Handling of connections to the server
+    server.listen() #server listening for connections
+    while True:
+        connect, adr = server.accept()  # This will store the address from where it came from while store it as a object
+        thread = threading.Thread(target=client_operation(), args=(connect, adr)) # allowing for more clients
+        thread.start()
+        print(f"Number of connected {threading.active_count() - 1}") # checks for number of clients connected to server
+
+print("Server connection found")
+
+"""
 class Server:
     server_socket = socket.socket()
     host = socket.gethostbyname("localhost")
@@ -13,4 +47,4 @@ class Server:
         print("Got connection from " + str(addr))
         client.send("Thank you for connecting".encode())
         client.close()
-
+"""
