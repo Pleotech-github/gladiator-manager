084008c3bcd03e3488f99bb769e92f6d1c8f1c22
TheGr8Chibi <43129699+TheGr8Chibi@users.noreply.github.com>
2020-11-19 10:49:49 +0100

    multiple clients

--- Client.py
+++ Client.py
@@ -1,4 +1,6 @@
 import socket
+import sys
+import time
 
 clientSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 serverAddress = ('localhost', 5000)
@@ -9,7 +11,14 @@ clientSocket.connect(serverAddress)
 while True:
     try:
         message = input()
-        message = message.encode()
-        clientSocket.send(message)
+        if message == 'exit':
+            print('Connection terminated')
+            print('Closing window')
+            clientSocket.close()
+            time.sleep(2)
+            sys.exit()
+        else:
+            clientSocket.send(str.encode(message))
     except:
         print('Error')
+        sys.exit()
--- Server.py
+++ Server.py
@@ -1,21 +1,27 @@
 import socket
-import sys
+import _thread
+
+
+def client(clientSckt, address):
+    try:
+        print('Client connected: ' + str(address))
+        while True:
+            data = clientSckt.recv(16)
+            data = data.decode()
+            print(data)
+            if not data:
+                break
+        clientSckt.close()
+    except:
+        print('Error')
 
 serverSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 serverAddress = ('localhost',5000)
 print('Starting server')
 serverSocket.bind(serverAddress)
-serverSocket.listen(1)
+serverSocket.listen(5)
 
 while True:
     print('Waiting for client')
-    connection, clientAddress = serverSocket.accept()
-
-    try:
-        print('Client connected: ' + str(clientAddress))
-        while True:
-            data = connection.recv(16)
-            data = data.decode()
-            print(data)
-    finally:
-        connection.close()
+    clientSocket, clientAddress = serverSocket.accept()
+    _thread.start_new_thread(client, (clientSocket, clientAddress))
